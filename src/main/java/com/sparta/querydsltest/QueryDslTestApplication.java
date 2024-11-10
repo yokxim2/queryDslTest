bbdb8c5a71913dccc8add8c7574af4d340e50491
yokxim <yooksi53@gmail.com>
2024-11-10 22:53:17 +0900

    Initial commit

---
+++ src/main/java/com/sparta/querydsltest/QueryDslTestApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.querydsltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDslTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryDslTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/querydsltest/QueryDslTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.querydsltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
