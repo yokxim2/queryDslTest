d1c6c5a511c089e8e6b8b9395df30e49fee8e4be
yokxim <yooksi53@gmail.com>
2024-11-11 10:35:29 +0900

    feat: queryDsl 기본 세팅

---
+++ src/main/java/com/sparta/querydsltest/entity/Team.java
@@ -0,0 +1,35 @@
+package com.sparta.querydsltest.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+	@Id @GeneratedValue
+	@Column(name = "team_id")
+	private Long id;
+
+	private String name;
+
+	@OneToMany(mappedBy = "team")
+	private List<Member> members = new ArrayList<>();
+
+	public Team(String name) {
+		this.name = name;
+	}
+}
