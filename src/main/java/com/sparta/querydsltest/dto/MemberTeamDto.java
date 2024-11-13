89a34b30718a41b6e04070e6f333b634ee401875
yokxim <yooksi53@gmail.com>
2024-11-13 14:42:44 +0900

    feat: 동적 쿼리와 성능 최적화 조회 - Builder 사용

---
+++ src/main/java/com/sparta/querydsltest/dto/MemberTeamDto.java
@@ -0,0 +1,23 @@
+package com.sparta.querydsltest.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MemberTeamDto {
+
+	private Long memberId;
+	private String username;
+	private int age;
+	private Long teamId;
+	private String teamName;
+
+	public MemberTeamDto(Long memberId, String username, int age, Long teamId, String teamName) {
+		this.memberId = memberId;
+		this.username = username;
+		this.age = age;
+		this.teamId = teamId;
+		this.teamName = teamName;
+	}
+}
--- src/main/java/com/sparta/querydsltest/repository/MemberJpaRepository.java
+++ src/main/java/com/sparta/querydsltest/repository/MemberJpaRepository.java
@@ -1,13 +1,19 @@
 package com.sparta.querydsltest.repository;
 
 import static com.sparta.querydsltest.entity.QMember.*;
+import static com.sparta.querydsltest.entity.QTeam.*;
+import static org.springframework.util.StringUtils.*;
 
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.querydsltest.dto.MemberSearchCondition;
+import com.sparta.querydsltest.dto.MemberTeamDto;
 import com.sparta.querydsltest.entity.Member;
 
 import jakarta.persistence.EntityManager;
@@ -49,6 +55,35 @@ public class MemberJpaRepository {
 		return queryFactory
 			.selectFrom(member)
 			.where(member.username.eq(username))
+			.fetch();
+	}
+
+	// Builder를 사용한 동적 쿼리 생성
+	public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition) {
+		BooleanBuilder builder = new BooleanBuilder();
+		if (hasText(condition.getUsername())) {
+			builder.and(member.username.eq(condition.getUsername()));
+		}
+		if (hasText(condition.getTeamName())) {
+			builder.and(team.name.eq(condition.getTeamName()));
+		}
+		if (condition.getAgeGoe() != null) {
+			builder.and(member.age.goe(condition.getAgeGoe()));
+		}
+		if (condition.getAgeLoe() != null) {
+			builder.and(member.age.loe(condition.getAgeLoe()));
+		}
+
+		return queryFactory
+			.select(Projections.fields(MemberTeamDto.class,
+				member.id,
+				member.username,
+				member.age,
+				team.id.as("teamId"),
+				team.name.as("teamName")))
+			.from(member)
+			.leftJoin(member.team, team)
+			.where(builder)
 			.fetch();
 	}
 }
