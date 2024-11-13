3cfb240fa941bd3a13f81a5f1295601b644abedc
yokxim <yooksi53@gmail.com>
2024-11-13 09:37:14 +0900

    test: 프로젝션과 결과 반환 - 기본

---
+++ src/test/java/com/sparta/querydsltest/QuerydslIntermediateTest.java
@@ -0,0 +1,75 @@
+package com.sparta.querydsltest;
+
+import static com.sparta.querydsltest.entity.QMember.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.querydsltest.entity.Member;
+import com.sparta.querydsltest.entity.Team;
+
+import jakarta.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional
+public class QuerydslIntermediateTest {
+
+	@Autowired
+	EntityManager em;
+
+	JPAQueryFactory queryFactory;
+
+	@Test
+	@BeforeEach
+	public void before() {
+		queryFactory = new JPAQueryFactory(em);
+		Team teamA = new Team("teamA");
+		Team teamB = new Team("teamB");
+		em.persist(teamA);
+		em.persist(teamB);
+
+		Member member1 = new Member("member1", 10, teamA);
+		Member member2 = new Member("member2", 20, teamA);
+
+		Member member3 = new Member("member3", 30, teamB);
+		Member member4 = new Member("member4", 40, teamB);
+		em.persist(member1);
+		em.persist(member2);
+		em.persist(member3);
+		em.persist(member4);
+	}
+
+	@Test
+	public void simpleProject() {
+		List<String> result = queryFactory
+			.select(member.username)
+			.from(member)
+			.fetch();
+
+		for (String s : result) {
+			System.out.println("s = " + s);
+		}
+	}
+
+	@Test
+	public void tupleProjection() {
+		List<Tuple> result = queryFactory
+			.select(member.username, member.age)
+			.from(member)
+			.fetch();
+
+		for (Tuple tuple : result) {
+			String username = tuple.get(member.username);
+			Integer age = tuple.get(member.age);
+			System.out.println("username = " + username);
+			System.out.println("age = " + age);
+		}
+	}
+}
