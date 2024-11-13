7a681d282a1204dded84d4cd6bdc47d24ea34610
yokxim <yooksi53@gmail.com>
2024-11-13 14:09:54 +0900

    feat: MemberJpaRepository test: MemberJpaRepositoryTest

--- src/main/java/com/sparta/querydsltest/QueryDslTestApplication.java
+++ src/main/java/com/sparta/querydsltest/QueryDslTestApplication.java
@@ -2,12 +2,22 @@ package com.sparta.querydsltest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
 
 @SpringBootApplication
 public class QueryDslTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QueryDslTestApplication.class, args);
+	}
+
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
 	}
 
 }
---
+++ src/test/java/com/sparta/querydsltest/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.sparta.querydsltest.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sparta.querydsltest.entity.Member;
+
+import jakarta.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+	@Autowired
+	EntityManager em;
+
+	@Autowired
+	MemberJpaRepository memberJpaRepository;
+
+	@Test
+	public void basicTest() {
+		Member member = new Member("member1", 10);
+		memberJpaRepository.save(member);
+
+		Member findMember = memberJpaRepository.findById(member.getId()).get();
+		assertThat(findMember).isEqualTo(member);
+
+		List<Member> result1 = memberJpaRepository.findAll();
+		assertThat(result1).containsExactly(member);
+
+		List<Member> result2 = memberJpaRepository.findByUsername("member1");
+		assertThat(result2).containsExactly(member);
+	}
+
+	@Test
+	public void basicQuerydslTest() {
+		Member member = new Member("member1", 10);
+		memberJpaRepository.save(member);
+
+		Member findMember = memberJpaRepository.findById(member.getId()).get();
+		assertThat(findMember).isEqualTo(member);
+
+		List<Member> result1 = memberJpaRepository.findAll_Querydsl();
+		assertThat(result1).containsExactly(member);
+
+		List<Member> result2 = memberJpaRepository.findByUsername_Querydsl("member1");
+		assertThat(result2).containsExactly(member);
+	}
+}
